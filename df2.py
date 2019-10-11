3ba786c4bf07d5f2745e7df841b75b83069f0887
João Marques <jotaveultra@gmail.com>
2019-10-10 23:35:44 -0300

    Ask which file to delete

--- df2.py
+++ df2.py
@@ -1,20 +1,21 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 """
 Usage:
-    df2 <path>... [--quiet --output=OUTPUT --like=LIKE]
+    df2 <path>... [--quiet --output=OUTPUT --like=LIKE --size]
 
 Options:
     -q --quiet
     -o --output=OUTPUT
     -h --help
     -l --like=LIKE
+    -s --size
 """
 
 from collections import defaultdict
 from docopt import docopt
 from functools import lru_cache
 from hashlib import md5
-from os import walk
+from os import remove, walk
 from os.path import join
 from pprint import pformat
 
@@ -63,9 +64,22 @@ if __name__ == '__main__':
     arguments = docopt(__doc__)
     file_name = arguments['--output']
     hd = df(arguments['<path>'], arguments['--quiet'], arguments['--like'])
-    hd = pformat({k:v for k, v in hd.items() if len(v) > 1})
+    rhd = {k:v for k, v in hd.items() if len(v) > 1}
+    fhd = pformat(rhd)
     if file_name:
         with open(file_name, 'w') as hf:
             hf.write(hd+'\n')
     else:
-        print(hd)
+        print(fhd)
+        if arguments['--size']:
+            print(len(rhd))
+    for dup in rhd.values():
+        duplicates = list(dup)
+        for i, file in enumerate(duplicates):
+            print(f"{i} - {file}")
+        try:
+            to_delete = int(input())
+            remove(duplicates[to_delete])
+            print()
+        except:
+            pass
